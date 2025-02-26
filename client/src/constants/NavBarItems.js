433c1376faf0bf93dfbf3b3581455851e70d1b02
Leyla Khairnabiyeva <leylaskh@code.edu.az>
2025-02-26 15:15:28 +0400

    Updated navbar layout and styling for new links

---
+++ client/src/constants/NavBarItems.js
@@ -0,0 +1,6 @@
+export const NavBarElements = [
+  { id: "1", name: "Home", url: "/" },
+  { id: "2", name: "Shop", url: "/shop" },
+  { id: "3", name: "Product", url: "/" },
+  { id: "4", name: "Contact Us", url: "/" },
+];
--- client/src/layout/header/Header.jsx
+++ client/src/layout/header/Header.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import styles from "./style.module.scss";
+import { Link } from "react-router";
+import { NavBarElements } from "@/constants/NavBarItems";
 
 const Header = () => {
   return (
     <header>
-      <div>
-        <h1 className={styles.logo}>3legant.</h1>
+      <div className={styles.root}>
+        {" "}
+        <div>
+          <h1 className={styles.logo}>3legant.</h1>
+        </div>
+        <div>
+          <ul className={styles.navlist}>
+            {NavBarElements &&
+              NavBarElements.map((item, index) => (
+                <li key={index} className={styles.navtext}>
+                  <Link to={item.url}>{item.name}</Link>
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
     </header>
   );
