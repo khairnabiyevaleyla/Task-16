45aad130b6c25abe0f25135a41adf885962a645e
Leyla Khairnabiyeva <leylaskh@code.edu.az>
2025-02-26 14:27:43 +0400

    Created initial query to fetch data from API

---
+++ client/src/http/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const AxiosInstance = axios.create({
+  baseURL: "http://localhost:1337/api",
+  timeout: 1000,
+});
+
+export const getApi = async (url) => {
+  try {
+    const response = await AxiosInstance.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const postApi = async (url, payload) => {
+  try {
+    const response = await AxiosInstance.post(url, payload);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,8 +9,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      retry: 1,
-      staleTime: 1000,
+      retry: 2,
     },
   },
 });
