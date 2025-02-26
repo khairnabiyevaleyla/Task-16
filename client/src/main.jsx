4d0009566c4ee3ea989c6253e6cba8f64209fb07
Leyla Khairnabiyeva <leylaskh@code.edu.az>
2025-02-26 14:22:05 +0400

    Added React Query Devtools for debugging

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,10 +15,15 @@ const queryClient = new QueryClient({
   },
 });
 
+const ShowDevToolsInDevelopment = import.meta.env.MODE === "development" && (
+  <ReactQueryDevtools initialIsOpen={false} />
+);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      {ShowDevToolsInDevelopment}
     </QueryClientProvider>
   </StrictMode>
 );
